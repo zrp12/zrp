3cd05689a87776a157a23a0394e9f19e3a176253
zrp152 <2022374925@qq.com>
2024-04-27 16:29:22 +0800

    循环

--- leetCode/String/while.js
+++ leetCode/String/while.js
@@ -1,2 +1,14 @@
 let arr = [1,2,3,4,5,6];
 
+// for(let i = 0; i < arr.length;i++){
+//     if(arr[i] < 4){
+//         arr[i] = arr[i] * 10;
+//     }
+// }
+// console.log(arr);
+  let i = 0;
+while(arr[i] < 4){
+    arr[i] = arr[i] * 10;
+    i++;
+}
+console.log(arr);
