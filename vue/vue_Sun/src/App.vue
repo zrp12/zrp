e9c6f630a90204bb372e5d8beb82e72348f7a2f9
zrp152 <2022374925@qq.com>
2024-06-23 22:32:16 +0800

    vue学习

---
+++ vue/vue_Sun/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+ <input v-model="message" placeholder="edit me">
+ <p>作者是: {{ message }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+ let message = ref('')
+</script>
+
+<!-- <style lang="css" scoped>
+img {
+  width: 500px;
+  height: 500px;
+  object-fit: cover;
+}
+.bd {
+  border: 8px solid #51bbd3;
+}
+</style> -->
